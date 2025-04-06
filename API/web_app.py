1af14e2dc002dbddaa4976bf2a73a3f002fc4e8e
Schlvf <axlzbot@gmail.com>
2025-04-05 23:23:16 -0500

    Removed CORS middleware since no frontend access is needed

--- API/web_app.py
+++ API/web_app.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from modules.redis.routers import redis_router
 from modules.spotify.routers import spotify_router
@@ -9,20 +8,6 @@ app = FastAPI()
 app.include_router(eventsub_router.router)
 app.include_router(spotify_router.router)
 app.include_router(redis_router.router)
-
-origins = [
-    "*",
-]
-origin_regex = ".*:*"
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_origin_regex=origin_regex,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get("/ping")
