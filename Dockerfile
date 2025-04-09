cb7cf7e7abee51a7b2d9da5320341a0e43fa1211
Schlvf <axlzbot@gmail.com>
2025-04-08 22:57:33 -0500

    Implemented a simple parcel project that builds static files so they can be loaded to Jinja templates as optimized as possible

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,22 @@
-FROM python:3.13.2-slim-bookworm
+FROM node:lts-bookworm-slim AS build-stage
 
-WORKDIR /
+WORKDIR /build
+
+COPY package*.json .
+RUN npm install
+
+COPY /static ./static
+RUN npm run build
+
+FROM python:3.13.2-slim-bookworm AS python-runtime
+
+WORKDIR /webapp
 
 COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY /src/. .
+COPY --from=build-stage /build/dist ./dist
 
 CMD ["python","main.py"]
--- src/api/web_app.py
+++ src/api/web_app.py
@@ -1,15 +1,39 @@
 from fastapi import FastAPI
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from modules.redis import redis_router
 from modules.spotify import spotify_router
 from modules.twitch import eventsub_router
 
+templates = Jinja2Templates(directory="dist")
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="dist"), name="static")
 app.include_router(eventsub_router)
 app.include_router(spotify_router)
 app.include_router(redis_router)
 
 
+@app.get("/", response_class=HTMLResponse)
+async def front(request: Request):
+    title = "Hello World"
+    description = "This was a value passed from python"
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"title": title, "description": description},
+    )
+
+
 @app.get("/ping")
 async def ping():
     return {"message": "Pong!"}
+
+
+@app.get("/{file_path:path}")
+async def redirect_to_static(file_path: str):
+    return RedirectResponse(url=f"/static/{file_path}")
